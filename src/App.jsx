4403a5e74f1110aabf6fc0cb6825c6e166bde6a6
Mubarak Awal <mubarak.awal@egym.com>
2024-09-17 19:13:39 +0200

    change landing page description

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR -hhhhh
         </p>
       </div>
       <p className="read-the-docs">
