ebed813cacb2d62915be4e483104dfe52db45c1c
Iman <bajalaniman@gmail.com>
2024-09-20 10:23:17 +0200

    adding meet your host component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MeetTheHost from "./feature/MeetTheHost";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,8 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <MeetTheHost />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
