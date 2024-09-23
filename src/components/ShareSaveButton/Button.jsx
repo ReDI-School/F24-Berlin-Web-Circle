4f14494b1b49bac70a6ddf5a958757c76322ca7b
alinaincodeland <alinacuznetov@gmail.com>
2024-09-24 00:38:19 +0200

    Change class names.

--- src/components/ShareSaveButton/Button.jsx
+++ src/components/ShareSaveButton/Button.jsx
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Button({ faIcon, label, onClick }) {
   return (
-    <button onClick={onClick}>
-      <div className="button-container">
-        <span className="icon">
-          <FontAwesomeIcon icon={faIcon} />
+    <button className="save-share-button" onClick={onClick}>
+      <div className="button-content">
+        <span>
+          <FontAwesomeIcon icon={faIcon} className="fa-icon" fill="false" />
         </span>
         {label}
       </div>
