d2f84f51aa1a9f75341af4dffd9d0e86bc2581cf
miklesch <m.kleinschmidt.dev@gmail.com>
2024-09-24 12:44:05 +0200

    Add Header component and its styling file

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from "./Header.module.css"
+import airbnbLogo from "/logo_airbnb.svg"
+import ToggleButtonsStaysExperiences from '../ToggleButtonsStaysExperiences/ToggleButtonsStaysExperiences'
+import ButtonBeAHost from '../ButtonBeAHost/ButtonBeAHost'
+import LanguageSelector from '../LanguageSelector/LanguageSelector'
+
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logoDiv}>
+                <a className={styles.airbnbHomeButton}
+                    href="/">
+                    <img src={airbnbLogo} className={styles.logoAirbnb} alt='The Logo of AirBnB' />
+                </a>
+            </div>
+            {/* {<ToggleButtonsStaysExperiences/> } */}
+
+
+
+            {<div className={styles.toggleButtonsStaysExperiences}>
+                <button className={styles.buttonStays}>Stays</button>
+                <button className={styles.buttonExperiences}>Experiences</button>
+            </div>}
+            <div className={styles.beAHostAndLanguageSelectorWrapper}>
+                <div className={styles.hostButtonDiv}>
+                    <ButtonBeAHost />
+                </div>
+                <div className={styles.languageSelectorDiv}>
+                    <LanguageSelector />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
