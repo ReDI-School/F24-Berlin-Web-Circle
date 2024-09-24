dfa78081192de8805f0acf3fc5eea3da3c66457c
alinaincodeland <alinacuznetov@gmail.com>
2024-09-24 18:44:59 +0200

    Add save and share buttons to product page.

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,27 @@
 import { useParams } from "react-router-dom";
+import IconButton from "../components/IconButton/IconButton";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const ProductPage = () =>
 {
 	const {productId} = useParams();	// Object Destructuring
 
+	function handleShare() {
+		console.log("handle share")
+	}
+
+	function handleSave() {
+		console.log("handle share")
+	}
+
 	return (
 	<div>
 		{`Dummy Product Page ${productId}`}
+		<div className="share-save-buttons-container">
+		<IconButton faIcon={faArrowUpFromBracket} label="Share" onClick={handleShare}/>
+		<IconButton faIcon={faHeart} label="Save" onClick={handleSave}/>
+		</div>
 	</div>
 	)
 }
