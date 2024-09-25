43358b8ea67497003a2b2c77eff84d8c034d5107
miklesch <m.kleinschmidt.dev@gmail.com>
2024-09-25 11:57:30 +0200

    Removed the Header component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PersonProfile from "./components/personProfile/PersonProfile";
-import Header from './components/Header/Header'
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header />
       <h1 data-testid="heading">Hello F24-Berlin-Web-Circle Typo fixed</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
