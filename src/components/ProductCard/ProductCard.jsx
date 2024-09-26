4a8fbb13d24573b4912c677a313c63f388408586
Kali <136571919+messkalina@users.noreply.github.com>
2024-09-26 16:34:03 +0200

    "Share" icon created and stylised exactly like in AC,

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -8,30 +8,22 @@ const ProductCard = ({ image, title, host, price }) => {
 
   return (
     <div className="product-card">
-    
+
       <button onClick={handleShare} className="share-button">
-        
-      <svg
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                role="img"
-                aria-label="share"
-                className="share-icon"
-    />
-      </button>               
+      <span class="share-icon"><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+        <path d="m27 18v9c0 1.1046-.8954 2-2 2h-18c-1.10457 0-2-.8954-2-2v-9m11-15v21m-10-11 9.2929-9.29289c.3905-.39053 1.0237-.39053 1.4142 0l9.2929 9.29289" fill="none"></path></svg>
+        </span>
+      </button>
       <img src={image} alt={title} className="product-image" />
 
       <div className="card-content">
         <div className="card-header">
           <h2 className="card-title">{title}</h2>
-          </div>
         </div>
-        <p className="card-host">{host}</p>
-        <p className="card-price">{price}</p>
       </div>
+      <p className="card-host">{host}</p>
+      <p className="card-price">{price}</p>
+    </div>
   );
 };
 
