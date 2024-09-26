c11499b033e43957b9b971f024281defb8557f75
miklesch <m.kleinschmidt.dev@gmail.com>
2024-09-26 13:15:48 +0200

    Delete unnecessary styling

--- src/components/LanguageSelector/LanguageSelector.jsx
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -6,7 +6,7 @@ import styles from "./LanguageSelector.module.css"
 const LanguageSelector = () => {
     return (
         <div className={styles.languageSelector}>
-            <a href="/language-selector" className={styles.labguageSelectorLink}>
+            <a href="/language-selector">
             <img src={globe_icon} className={styles.globe_icon} alt="A globe icon as a link to select the language" />
             </a>
         </div>
