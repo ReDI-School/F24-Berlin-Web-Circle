00fb67ceaa09d4417ec93fad7e10d2cf367007bc
Bohdan Hrabynskyi <bogdan.grabinskij@gmail.com>
2024-09-27 14:03:58 +0200

    Create HostSummary component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import ReservationCard from "../components/ReservationCard/ReservationCard";
+import HostSummary from "../components/HostSummary/HostSummary";
 
 const ProductPage = () => {
   const { productId } = useParams(); // Object Destructuring
@@ -8,7 +9,15 @@ const ProductPage = () => {
     <div>
       {`Dummy Product Page ${productId}`}
       <ReservationCard />
+      <br />
+      <HostSummary 
+          hostName="Raus"
+          hostingDuration={1}
+          role="Superhost"
+          profilePicUrl="https://a0.muscache.com/im/pictures/user/d62627ea-ea22-4cf1-b38a-152f1f86a9ed.jpg"
+      />
     </div>
+    
   );
 };
 
