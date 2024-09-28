ea99b33eecdfabe4cf68ca692a9fb350f141a032
samkons <konzisamuel@gmail.com>
2024-09-28 13:16:44 +0200

    added changes to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PersonProfile from "./components/personProfile/PersonProfile";
-import FavoriteStay from "./components/FavouriteStay/FavoriteStay";
 import { Link } from 'react-router-dom'
 
 function App() {
