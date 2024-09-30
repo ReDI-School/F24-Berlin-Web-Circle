b1519f2dbe70c769f08a571aa49486dfe6976959
BhagyaPrasadSamarathunga <bhagyasamarathunga@gmail.com>
2024-09-30 02:45:25 +0200

    Create Destination search popup (#73)
    * Create Destination search popup
    
    * Address merge comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import AddGuestsPopUp from "../AddGuestsPopUp/AddGuestsPopUp";
+import DestinationPopUp from '../DestinationPopUp/DestinationPopUp'
+
 const Header = () =>
 {
+	const handelRegionClick = (item) => {
+	}
+
 	return (
 		<div>
 			<header>
 				Dummy Header
 			</header>
 			<AddGuestsPopUp/>
+          	<DestinationPopUp title = 'Search by region' onClick = {(e)=>handelRegionClick(e)}/>
 		</div>
 
 	)
