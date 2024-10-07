1aa2fafd781d61d58859a751a0044b9b1ea0c879
mohammad salavati <mohammadsalavati@mohammads-MBP.fritz.box>
2024-10-07 19:48:01 +0200

    adds component to product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import ReservationCard from "../components/ReservationCard/ReservationCard";
-
+import DateIncrementsButtonForTheCalendar from "../components/DataIncrementsButtonForTheCalendar/DataIncrementsButtonForTheCalendar";
 const ProductPage = () => {
   const { productId } = useParams(); // Object Destructuring
 
   return (
     <div>
       {`Dummy Product Page ${productId}`}
+      <DateIncrementsButtonForTheCalendar />
       <ReservationCard />
     </div>
   );
