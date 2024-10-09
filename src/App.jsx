da28a550bfa1660c6e6a92452710f4f83a8aa370
miklesch <m.kleinschmidt.dev@gmail.com>
2024-10-09 13:18:57 +0200

    Set Link from ProductCard to ProductPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import "./App.css";
 import ProductCard from "./components/ProductCard/ProductCard";
 import CalendarToggle from "./components/calendarToggle/CalendarToggle";
@@ -6,11 +5,28 @@ import PersonProfile from "./components/personProfile/PersonProfile";
 import SearchBar from "./components/searchBar/SearchBar";
 
 function App() {
-  const places = [
+  /* const places = [
     { id: "1", name: "Idyllic house by the sea" },
     { id: "2", name: "Studio Zempow / ecological wooden house / photo studio" },
     { id: "3", name: "Funen's best ocean view" },
+  ]; */
+
+  const places = [
+    { id: "1", 
+      title: "Idyllic house by the sea", 
+      host: "Hosted by Wendy and Elisa",
+      price: "Coming soon",
+      image: "src/assets/4f7a276e-9995-4b32-bda6-300de0619b25.webp"},
+    { id: "2", title: "Studio Zempow / ecological wooden house / photo studio" },
+    { id: "3", title: "Funen's best ocean view" },
+    { id: "4", title: "Cozy Apartment in City Center"} 
   ];
+
+  
+  
+  
+  
+
 
   const handleAirbnbSearch = ({ location, checkIn, checkOut, guests }) => {
     // Logic for home search
@@ -42,22 +58,23 @@ function App() {
         rating={4.85}
         yearsHosting={1}
         />
-       <ProductCard 
-        image={"src/assets/4f7a276e-9995-4b32-bda6-300de0619b25.webp"} 
-        title={"Cozy Apartment in City Center"}
-        host={"Hosted by Wendy and Elisa"}
-        price={"Coming soon"}
-        place={places}
-      
-      />
+        
+        <div>
+        {places.map((place) => (
+        <ProductCard 
+        key={place.id}
+        place={place}
+      />  
+        ))}
+       </div>
       {/* FOR TESTING */}
-      {places.map((place) => {
+      {/* {places.map((place) => {
         return (
           <Link to={`/rooms/${place.id}`} key={place.id}>
             <div>{`${place.id} - ${place.name}`}</div>
           </Link>
         );
-      })}
+      })} */}
     </>
     
   );
