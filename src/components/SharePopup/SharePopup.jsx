6c953e07b336e19fbaf33126a7a12e5a1d5fa441
006080 <nataliyarodionova28@gmail.com>
2024-10-11 15:57:28 +0200

    modal-thumble added

--- src/components/SharePopup/SharePopup.jsx
+++ src/components/SharePopup/SharePopup.jsx
@@ -25,7 +25,7 @@ const SharePopup = ({ onClick }) => {
       <div className="modal-content">
         <div className="modal-info">
           <img
-            src="https://via.placeholder.com/50"
+            src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=1200&im_q=highq"
             alt="Doja Cat Session"
             className="modal-thumbnail"
           />
