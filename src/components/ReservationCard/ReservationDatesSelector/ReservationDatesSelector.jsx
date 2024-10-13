be42c1c39b6b273f1a48e19c69b2e4838d346297
Bohdan Hrabynskyi <bogdan.grabinskij@gmail.com>
2024-10-13 12:47:10 +0200

    Refactore date selection logic in ReservationDatesSelector and DatePicker components to enable proper check-in/check-out date handling and ensure calendar closes and opens again when both dates are selected.

--- src/components/ReservationCard/ReservationDatesSelector/ReservationDatesSelector.jsx
+++ src/components/ReservationCard/ReservationDatesSelector/ReservationDatesSelector.jsx
@@ -2,6 +2,7 @@ import useOutsideClick from '../../../hooks/useOutsideClick'
 import DatePicker from '../DatePicker/DatePicker'
 import { KeyboardIcon } from '../../../icons/KeyboardIcon'
 import styles from './ReservationDatesSelector.module.css'
+import { useEffect, useState } from 'react'
 
 const ReservationDatesSelector = ({
   setCheckInDate,
@@ -12,6 +13,48 @@ const ReservationDatesSelector = ({
 }) => {
   const calendarRef = useOutsideClick(() => toggleShowCalendar(false))
 
+  const [userSelectedCheckIn, setUserSelectedCheckIn] = useState(false)
+  const [userSelectedCheckOut, setUserSelectedCheckOut] = useState(false)
+
+  const handleCheckInChange = (date) => {
+    setCheckInDate(date)
+    setUserSelectedCheckIn(true)
+  }
+
+  const handleCheckOutChange = (date) => {
+    setCheckOutDate(date)
+    setUserSelectedCheckOut(true)
+  }
+
+  useEffect(() => {
+    if (
+      userSelectedCheckIn &&
+      userSelectedCheckOut &&
+      checkInDate < checkOutDate
+    ) {
+      if (checkInDate > checkOutDate) {
+        alert('Check-in date must be before check-out date')
+      }
+      toggleShowCalendar(false)
+      resetSelectionFlags()
+    }
+  }, [
+    checkInDate,
+    checkOutDate,
+    toggleShowCalendar,
+    userSelectedCheckIn,
+    userSelectedCheckOut,
+  ])
+
+  const resetSelectionFlags = () => {
+    setUserSelectedCheckIn(false)
+    setUserSelectedCheckOut(false)
+  }
+  console.log('checkInDate', checkInDate)
+  console.log('checkoutDate', checkOutDate)
+  console.log('userSelectedCheckIn', userSelectedCheckIn)
+  console.log('userSelectedCheckOut', userSelectedCheckOut)
+
   return (
     <div className={styles.selectorContainer} ref={calendarRef}>
       <div className={styles.datePickerWrapper}>
@@ -19,24 +62,34 @@ const ReservationDatesSelector = ({
           <h2>Select dates</h2>
           <span>Add your travel dates for exact pricing</span>
         </div>
-        <DatePicker />
+        <DatePicker
+          checkInDate={checkInDate}
+          checkOutDate={checkOutDate}
+          setCheckInDate={setCheckInDate}
+          setCheckOutDate={setCheckOutDate}
+          setUserSelectedCheckIn={setUserSelectedCheckIn}
+          setUserSelectedCheckOut={setUserSelectedCheckOut}
+        />
       </div>
       <div className={styles.inputsContainer}>
         <input
           type="date"
           className={styles.dateInput}
           value={checkInDate}
-          onChange={(e) => setCheckInDate(e.target.value)}
+          onChange={(e) => handleCheckInChange(e.target.value)}
           required
         />
+        {checkInDate && (
         <input
           type="date"
           className={styles.dateInput}
           value={checkOutDate}
-          onChange={(e) => setCheckOutDate(e.target.value)}
+          onChange={(e) => handleCheckOutChange(e.target.value)}
           required
           min={checkInDate}
         />
+        )
+      }
       </div>
       <div className={styles.buttonsContainer}>
         <button className={styles.shortcutsPopupButton}>
@@ -44,7 +97,16 @@ const ReservationDatesSelector = ({
         </button>
         <div className={styles.clearToggleBtnWrapper}>
           <div className={styles.clearDatesButton}>
-            <button className={styles.clearDatesButton}>Clear dates</button>
+            <button
+              className={styles.clearDatesButton}
+              onClick={() => {
+                setCheckInDate('')
+                setCheckOutDate('')
+                resetSelectionFlags()
+              }}
+            >
+              Clear dates
+            </button>
           </div>
           <div className={styles.cancelButton}>
             <button onClick={() => toggleShowCalendar(false)}>Close</button>
