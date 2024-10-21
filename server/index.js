02b09bb4db9e62117b43510e96d45a125ac33335
Iman <bajalaniman@gmail.com>
2024-10-21 20:22:46 +0200

    Adding endpoint for data specific place

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const products = require("./src/data/places.json");
 const app = express();
 const PORT = 8800;
 
@@ -28,12 +28,12 @@ app.post("/savePlace", (req, res) => {
 });
 
 // import Routes
-const placesRoutes = require('./routes/places');
-const destinationsRoutes = require('./routes/destinations');
+const placesRoutes = require("./routes/places");
+const destinationsRoutes = require("./routes/destinations");
 
 // Use Routes
-app.use('/places', placesRoutes); 
-app.use('/destinations', destinationsRoutes); 
+app.use("/places", placesRoutes);
+app.use("/destinations", destinationsRoutes);
 
 app.listen(PORT, (error) => {
   if (error) console.log("Error starting the server:", error);
