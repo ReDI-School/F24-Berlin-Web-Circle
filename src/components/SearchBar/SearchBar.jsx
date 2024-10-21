b7df227a15d7cf636b33df905b67fbfdef9b2444
miklesch <m.kleinschmidt.dev@gmail.com>
2024-10-21 14:52:09 +0200

    Using ModalPopUp for Calendar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
 import Calendar from "../Calendar/calendar";
+import ModalPopUp from "../ModalPopUp/ModalPopUp";
 
 const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, checkOut: initialCheckOut, guests: initialGuests, onSearch }) => {
   const [location, setLocation] = useState("");
@@ -22,75 +23,82 @@ const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, che
   };
 
   return (
-<>
-    <div className={styles.searchBar}>
-      <div className={styles.inputContainerWhere}>
-        <span className={styles.label}>Where</span>
-        <input
-          className={styles.inputTextPlaceholder}
-          type="text"
-          placeholder="Search destinations"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+    <>
+      <div className={styles.searchBar}>
+        <div className={styles.inputContainerWhere}>
+          <span className={styles.label}>Where</span>
+          <input
+            className={styles.inputTextPlaceholder}
+            type="text"
+            placeholder="Search destinations"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
 
-        />
-      </div>
-      <div className={styles.separator}></div>
-      {searchType === "stays" ? (
-        <>
-          <div className={styles.inputContainerCheckIn}>
-            <span className={styles.label}>Check in</span>
-            <input
-              type="text"
-              placeholder="Add dates"
-              value={checkIn}
-              onChange={(e) => setCheckIn(e.target.value)}
-              onFocus={toggleCalendar}
-            />
-            
-          </div>
-          <div className={styles.separator}></div>
-          <div className={styles.inputContainerCheckOut}>
-            <span className={styles.label}>Check out</span>
-            <input
-              type="text"
-              placeholder="Add dates"
-              value={checkOut}
-              onChange={(e) => setCheckOut(e.target.value)}
-            />
-          </div>
+          />
+        </div>
+        <div className={styles.separator}></div>
+        {searchType === "stays" ? (
+          <>
+            <div className={styles.inputContainerCheckIn}>
+              <span className={styles.label}>Check in</span>
+              <input
+                type="text"
+                placeholder="Add dates"
+                value={checkIn}
+                onChange={(e) => setCheckIn(e.target.value)}
+                onFocus={toggleCalendar}
+              />
 
-        </>
-      ) : (
-        <>
-          <div className={styles.inputContainerDate}>
-            <span className={styles.label}>Date</span>
-            <input
-              type="text"
-              placeholder="Add dates"
-              value={date}
-              onChange={(e) => setDates(e.target.value)}
-            />
-          </div>
-        </>
-      )}
-      <div className={styles.separator}></div>
-      <div className={styles.inputContainerWho}>
-        <span className={styles.label}>Who</span>
-        <input
-          type="text"
-          placeholder="Add guests"
-          value={guests}
-          onChange={(e) => setGuests(e.target.value)}
-        />
+            </div>
+            <div className={styles.separator}></div>
+            <div className={styles.inputContainerCheckOut}>
+              <span className={styles.label}>Check out</span>
+              <input
+                type="text"
+                placeholder="Add dates"
+                value={checkOut}
+                onChange={(e) => setCheckOut(e.target.value)}
+                onFocus={toggleCalendar}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.inputContainerDate}>
+              <span className={styles.label}>Date</span>
+              <input
+                type="text"
+                placeholder="Add dates"
+                value={date}
+                onChange={(e) => setDates(e.target.value)}
+                onFocus={toggleCalendar}
+              />
+            </div>
+          </>
+        )}
+        <div className={styles.separator}></div>
+        <div className={styles.inputContainerWho}>
+          <span className={styles.label}>Who</span>
+          <input
+            type="text"
+            placeholder="Add guests"
+            value={guests}
+            onChange={(e) => setGuests(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleSearch} className={styles.circleButton}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
       </div>
-      <div>
-        <button onClick={handleSearch} className={styles.circleButton}>
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </div>
-    </div>
-    {showCalendar && <Calendar />}
+      <ModalPopUp
+        isVisible={showCalendar}
+        onCloseClick={() => setShowCalendar(false)}
+        popUpDimension={{ width: "600px", left: "30%" }}
+      >
+        <Calendar />
+      </ModalPopUp>
     </>
   );
 };
