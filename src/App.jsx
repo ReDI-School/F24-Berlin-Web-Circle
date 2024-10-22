6c54108f2898b03fb3e72b6ae238e8c2fa2d5ac6
006080 <nataliyarodionova28@gmail.com>
2024-10-22 22:37:06 +0200

    carusel+shareModal debuging

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         console.error(`Something went wrong. ${error.message}.`)
       );
   }, []);
+  
   const handlePlaceClick = (placeId) => {
     setSelectPlaceId(placeId);
     console.log(placeId);
@@ -38,31 +39,22 @@ function App() {
         <CalendarToggle />
       </div>
 
-      {/* <ProductCard
-        images={[
-          "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/a766e0e9-1e6f-4b88-b8d5-ce12375c6de8.png?im_w=1200&im_q=highq",
-          "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/71d534a9-6699-4fe0-ad82-a9aaf0450b56.png?im_w=1200&im_q=highq",
-          "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/3f7a34a4-0052-4d5d-8e81-a75667f48a70.png?im_w=1200&im_q=highq",
-          "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/ebbcf8a7-a619-4284-bf02-c8dc208c2670.png?im_w=1200&im_q=highq",
-          "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/81d2ee4c-7056-4334-a2e7-3a0e4e3d63ca.png?im_w=1200&im_q=highq",
-        ]}
-        title="Cozy Apartment in City Center"
-        host="Hosted by Wendy and Elisa"
-        price="Coming soon"
-      /> */}
-
-      <div>
+      <div className="grid">
         {places.map((place) => {
           if (!place.id) return null;
           return (
             <div key={place.id} onClick={() => handlePlaceClick(place.id)}>
-              <Link to={`/rooms/${place.id}`} key={place.id}>
-                <ProductCard
-                  images={place.images}
-                  title={place.title}
-                  host={place.host}
-                  price={place.price}
-                />
+              <ProductCard
+                images={place.images}
+                title={place.title}
+                host={place.host}
+                price={place.price}
+              />
+              {/* Wrap only the text content in Link */}
+              <Link to={`/rooms/${place.id}`} className="productLink">
+                <h2>{place.title}</h2>
+                <p>{place.host}</p>
+                <p>{place.price}</p>
               </Link>
             </div>
           );
