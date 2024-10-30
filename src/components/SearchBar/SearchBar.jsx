ef5ba786269138ccfe340b408cb4944e3409fa1c
Bohdan Hrabynskyi <bogdan.grabinskij@gmail.com>
2024-10-30 22:31:45 +0100

    Replace inputs with divs to display state values like in original AirBnB

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,8 +7,8 @@ import CalendarToggle from "../calendarToggle/CalendarToggle";
 
 const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, checkOut: initialCheckOut, guests: initialGuests, onSearch }) => {
   const [location, setLocation] = useState("");
-  const [checkIn, setCheckIn] = useState(initialCheckIn || "");
-  const [checkOut, setCheckOut] = useState(initialCheckOut || "");
+  const [checkIn, setCheckIn] = useState(initialCheckIn || "Add dates");
+  const [checkOut, setCheckOut] = useState(initialCheckOut || "Add dates");
   const [guests, setGuests] = useState(initialGuests || "");
   const [date, setDates] = useState(initialDate || "");
 
@@ -16,7 +16,7 @@ const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, che
   const toggleCalendar = () => {
     setShowCalendar(!showCalendar);
   }
-
+console.log('searchType', searchType)
   const handleSearch = () => {
     //search logic here
     onSearch({ location, checkIn, checkOut, date, guests });
@@ -41,24 +41,12 @@ const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, che
           <>
             <div className={styles.inputContainerCheckIn} onClick={toggleCalendar}>
               <span className={styles.label}>Check in</span>
-              <input
-                type="text"
-                placeholder="Add dates"
-                value={checkIn}
-                onChange={(e) => setCheckIn(e.target.value)}
-                onFocus={toggleCalendar}
-              />
+              <span className={styles.checkInText}>{checkIn}</span>
             </div>
             <div className={styles.separator}></div>
-            <div className={styles.inputContainerCheckOut}>
+            <div className={styles.inputContainerCheckOut} onClick={toggleCalendar}>
               <span className={styles.label}>Check out</span>
-              <input
-                type="text"
-                placeholder="Add dates"
-                value={checkOut}
-                onChange={(e) => setCheckOut(e.target.value)}
-                onFocus={toggleCalendar}
-              />
+              <span className={styles.checkOutText}>{checkOut}</span>
             </div>
             {showCalendar && (
               <div className={styles.calendarWrapper}>
@@ -69,16 +57,15 @@ const SearchBar = ({ searchType, date: initialDate, checkIn: initialCheckIn, che
           </>
         ) : (
           <>
-            <div className={styles.inputContainerDate}>
+            <div className={styles.inputContainerDate} onClick={toggleCalendar}>
               <span className={styles.label}>Date</span>
-              <input
-                type="text"
-                placeholder="Add dates"
-                value={date}
-                onChange={(e) => setDates(e.target.value)}
-                onFocus={toggleCalendar}
-              />
+              <span className={styles.checkInText}>{checkIn}</span>
             </div>
+            {showCalendar && (
+              <div className={styles.calendarWrapper}>
+                <Calendar />
+              </div>
+            )}
           </>
         )}
         <div className={styles.separator}></div>
