1e9afeae30f16ec27899f80a98a282bbe3ee0e38
Vidushani Nishshanka <dvnishshanka@gmail.com>
2024-11-01 01:31:34 +0100

    Update Search function to latest places.json and fix bug related to dates

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <ProductPage />,
       },
       {
-        path: "s/:destination/homes",
+        path: "s/:region/homes",
         element: <FilteredPlacesPage />,
       },
     ],
