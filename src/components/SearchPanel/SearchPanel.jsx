687a3f7bcb448d83ea74825ef253603f03f1198f
orYoffe <or.yoffe@contentful.com>
2024-11-19 20:40:39 +0100

    fix: import statement

--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -6,49 +6,50 @@ import { useState } from "react";
 import { createSearchParams, useNavigate } from "react-router-dom";
 
 const SearchPanel = () => {
-  const [searchType, setSearchType] = useState("stays");
-  const navigate = useNavigate();
-
-  const toggleSearchType = (type) => {
-    setSearchType(type);
-  };
-  
-  const handleAirbnbSearch = ({
-    location: region,
-    checkIn,
-    checkOut,
-    guests,
-  }) => {
+    const [searchType, setSearchType] = useState("stays");
+    const navigate = useNavigate();
     
-    const searchQueries = {
-      region,
-      checkIn,
-      checkOut,
-      guests,
+    const toggleSearchType = (type) => {
+        setSearchType(type);
     };
     
-    if (region) {
-      navigate({
-        pathname: `/s/${region}/homes`,
-        search: createSearchParams(searchQueries).toString(),
-      });
-    }
-
-  return (
-    <div>
-      <div className={styles.toggleButtonsContainer}>
-        <ToggleButtonsStaysExperiences toggleSearchType={toggleSearchType} />
-      </div>
-      <div className={styles.searchBarContainer}>
-        <SearchBar
-          searchType={searchType}
-          checkIn=""
-          checkOut=""
-          guests=""
-          onSearch={handleAirbnbSearch}
-        />
-      </div>
-    </div>
-  );
+    const handleAirbnbSearch = ({
+        location: region,
+        checkIn,
+        checkOut,
+        guests,
+    }) => {
+        
+        const searchQueries = {
+            region,
+            checkIn,
+            checkOut,
+            guests,
+        };
+        
+        if (region) {
+            navigate({
+                pathname: `/s/${region}/homes`,
+                search: createSearchParams(searchQueries).toString(),
+            });
+        }
+    };
+        
+        return (
+            <div>
+            <div className={styles.toggleButtonsContainer}>
+            <ToggleButtonsStaysExperiences toggleSearchType={toggleSearchType} />
+            </div>
+            <div className={styles.searchBarContainer}>
+            <SearchBar
+            searchType={searchType}
+            checkIn=""
+            checkOut=""
+            guests=""
+            onSearch={handleAirbnbSearch}
+            />
+            </div>
+            </div>
+        );
 };
 export default SearchPanel;
