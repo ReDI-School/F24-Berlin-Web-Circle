b619845cde69210894e806503c76d5cfcba78b02
samkons <konzisamuel@gmail.com>
2024-11-28 16:54:13 +0100

    changes from tuesday

--- server/tests/places.test.js
+++ server/tests/places.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const chai = require('chai');
 const expect = chai.expect;
-const { before,  describe, it } = require('mocha');
+// const { before,  describe, it } = require('mocha');
 
 const app = require('../index.js');
 
@@ -14,4 +14,11 @@ describe("Test places endpoints", ()=>{
         const res = await request(app).get("/places");
         expect(res.status).to.equal(200);
     });
+
+    it("should return error when we have guest less than 1", async ()=>{
+        const res = await request(app).get("/places?guests=-5");
+        expect(res.status).to.not.equal(200);
+    });
+
+   
 });
